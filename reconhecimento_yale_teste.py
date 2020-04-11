b933ab79230c4b170329d3e7e9f56da5c798b8e1
Emerson Duarte <raasjava@gmail.com>
2020-04-11 17:28:07 -0300

    Treino reconhecendo alg facial

--- reconhecimento_yale_teste.py
+++ reconhecimento_yale_teste.py
@@ -4,17 +4,23 @@ import _pickle as cPickle
 import cv2
 import dlib
 import numpy as np
+from PIL import Image
 
 detectorFace = dlib.get_frontal_face_detector()
 detectorPontos = dlib.shape_predictor("recursos/shape_predictor_68_face_landmarks.dat")
 reconhecimentoFacial = dlib.face_recognition_model_v1("recursos/dlib_face_recognition_resnet_model_v1.dat")
-indices = np.load("recursos/indices_rn.pickle")
-descritoresFaciais = np.load("recursos/descritores_rn.npy")
+indices = np.load("recursos/indices_yale.pickle")
+descritoresFaciais = np.load("recursos/descritores_yale.npy")
 limiar = 0.5
+totalFaces = 0
+totalAcertos = 0
 
-for arquivo in glob.glob(os.path.join("fotos", "*.jpg")):
-    imagem = cv2.imread(arquivo)
-    facesDetectadas = detectorFace(imagem, 2)
+for arquivo in glob.glob(os.path.join("yalefaces/teste", "*.gif")):
+    imagemFace = Image.open(arquivo).convert('RGB')
+    imagem = np.asarray(imagemFace, 'uint8')
+    idatual = int(os.path.split(arquivo)[1].split(".")[0].replace("subject", ""))
+    totalFaces +=1
+    facesDetectadas = detectorFace(imagem, 3)
     for face in facesDetectadas:
         e, t, d, b = (int(face.left()), int(face.top()), int(face.right()), int(face.bottom()))
         pontosFaciais = detectorPontos(imagem, face)
@@ -27,13 +33,23 @@ for arquivo in glob.glob(os.path.join("fotos", "*.jpg")):
         #print("Distancias {}".format(distancias))
         minimo = np.argmin(distancias)
         distanciaMinima = distancias[minimo]
+
         if distanciaMinima <= limiar:
             nome = os.path.split(indices[minimo])[1].split(".")[0]
+            idprevisto = int(os.path.split(indices[minimo])[1].split(".")[0].replace("subject", ""))
+            if idprevisto == idatual:
+                totalAcertos +=1
         else:
-            nome = ' '
-        cv2.rectangle(imagem, (e, t), (d, b), (0, 255, 255), 2)
+            nome = 'Desconhecido'
+
+        print("idatual: {} idprevisto: {}".format(idatual, idprevisto))
+        cv2.rectangle(imagem, (e, t), (d, b), (0, 0, 255), 2)
         texto = "{} {:.4f}".format(nome, distanciaMinima)
-        cv2.putText(imagem, texto, (d, t), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0, 255, 255))
+        cv2.putText(imagem, texto, (d, t), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0, 0, 255))
 
     cv2.imshow("Detector Hog", imagem)
     cv2.waitKey(0)
+
+percentualAcerto = (totalAcertos / totalFaces) * 100
+print("Percentual de acertos: {}".format(percentualAcerto))
+cv2.destroyAllWindows()
