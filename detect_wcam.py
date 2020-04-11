b29ff4e9a5a9c772a4225ce1796a50692d2b0976
Emerson Duarte <raasjava@gmail.com>
2020-04-11 14:16:10 -0300

    Usando dlib para nomear as imagens

--- detect_wcam.py
+++ detect_wcam.py
@@ -14,12 +14,13 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.2, 5)
-    print(len(faces))
+    #print(len(faces))
 
     for(x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) == ord('q'):
         break
--- reconhecimentorn_teste.py
+++ reconhecimentorn_teste.py
@@ -10,6 +10,7 @@ detectorPontos = dlib.shape_predictor("recursos/shape_predictor_68_face_landmark
 reconhecimentoFacial = dlib.face_recognition_model_v1("recursos/dlib_face_recognition_resnet_model_v1.dat")
 indices = np.load("recursos/indices_rn.pickle")
 descritoresFaciais = np.load("recursos/descritores_rn.npy")
+limiar = 0.5
 
 for arquivo in glob.glob(os.path.join("fotos", "*.jpg")):
     imagem = cv2.imread(arquivo)
@@ -20,8 +21,19 @@ for arquivo in glob.glob(os.path.join("fotos", "*.jpg")):
         descritorFacial = reconhecimentoFacial.compute_face_descriptor(imagem, pontosFaciais)
         listaDescritorFacial = [fd for fd in descritorFacial]
         npArrayDescritorFacial = np.asarray(listaDescritorFacial,  dtype=np.float64)
-        npArrayDescritorFacial = npArrayDescritorFacial[np.axis, :]
+        npArrayDescritorFacial = npArrayDescritorFacial[np.newaxis, :]
 
+        distancias = np.linalg.norm(npArrayDescritorFacial - descritoresFaciais, axis=1)
+        #print("Distancias {}".format(distancias))
+        minimo = np.argmin(distancias)
+        distanciaMinima = distancias[minimo]
+        if distanciaMinima <= limiar:
+            nome = os.path.split(indices[minimo])[1].split(".")[0]
+        else:
+            nome = ' '
         cv2.rectangle(imagem, (e, t), (d, b), (0, 255, 255), 2)
-    cv2.imshow("Detector Hog", imagem)
-    cv2.waitKey(0)
+        texto = "{} {:.4f}".format(nome, distanciaMinima)
+        cv2.putText(imagem, texto, (d, t), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0, 255, 255))
+
+cv2.imshow("Detector Hog", imagem)
+cv2.waitKey(0)
